a5617c3f63ff09b2ee3544cbf5347b58d2c5045d
Pumpsuki <71671417+PartnerCapPikachu@users.noreply.github.com>
2024-10-30 00:37:15 -0400

    small changes/skip haxe logo

--- source/Main.hx
+++ source/Main.hx
@@ -1,37 +1,37 @@
 package;
 
-import debug.ALSoftConfig; // audio fix
+import debug.ALSoftConfig; //audio fix
 
 import openfl.events.UncaughtErrorEvent;
-import haxe.io.Path;
 
 class Main extends openfl.display.Sprite {
 	public function new():Void {
 		super();
-		addChild(new flixel.FlxGame(FlxG.width, FlxG.height, states.IntroState #if (flixel < '5.0.0') 1, #end, 60, 60, false, false));
+		addChild(new flixel.FlxGame(FlxG.width, FlxG.height, states.IntroState #if (flixel < '5.0.0') 1, #end, 60, 60, true, false));
 		addChild(new debug.FPSCounter());
+		openfl.Lib.current.loaderInfo.uncaughtErrorEvents.addEventListener(UncaughtErrorEvent.UNCAUGHT_ERROR, onCrash);
+	}
 
-		var location:String = './crash/';
-		openfl.Lib.current.loaderInfo.uncaughtErrorEvents.addEventListener(UncaughtErrorEvent.UNCAUGHT_ERROR, (uError:UncaughtErrorEvent) -> {
-			var errorPath:String = location + 'FNaSH_' + Date.now().toString().replace(' ', '_').replace(':', '\'');
+	var crashLocation:String = './crash/';
+	function onCrash(error:UncaughtErrorEvent):Void {
+		var errorPath:String = crashLocation + 'FNaSH_' + Date.now().toString().replace(' ', '_').replace(':', '\'');
 
-			var errorMessage:String = '';
-			for (i in haxe.CallStack.exceptionStack(true)) {
-				switch (i) {
-					case FilePos(s, file, line, column):
-						errorMessage += '$file (line $line)\n';
-					default:
-						trace(i);
-				}
+		var errorMessage:String = '';
+		for (i in haxe.CallStack.exceptionStack(true)) {
+			switch (i) {
+				case FilePos(s, file, line, column):
+					errorMessage += '$file (line $line)\n';
+				default:
+					trace(i);
 			}
-			errorMessage += '\n\nUncaught Error: ' + uError.error + '\nCRASH HANDLER ORIGINALL WRITTEN BY sqirra-rng';
+		}
+		errorMessage += '\n\nUncaught Error: ' + error.error + '\nCRASH HANDLER ORIGINALLY WRITTEN BY sqirra-rng';
 
-			if (!FileSystem.exists(location))
-				FileSystem.createDirectory(location);
+		if (!FileSystem.exists(crashLocation))
+			FileSystem.createDirectory(crashLocation);
 
-			File.saveContent(errorPath, '$errorMessage\n');
-			lime.app.Application.current.window.alert(errorMessage, 'Uncaught Error!');
-			Sys.exit(1);
-		});
+		File.saveContent(errorPath, '$errorMessage\n');
+		lime.app.Application.current.window.alert(errorMessage, 'Uncaught Error!');
+		Sys.exit(1);
 	}
 }
